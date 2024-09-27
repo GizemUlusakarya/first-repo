46c3db1aa9224a9c744a0146b78bb8bda4ec51a4
GizemUlusakarya <gizem.techpro@gmail.com>
2024-09-27 18:18:54 +0100

    v3

--- src/main/java/first_git_package/MyGitClass2.java
+++ src/main/java/first_git_package/MyGitClass2.java
@@ -5,18 +5,7 @@ public class MyGitClass2 {
     public static void main(String[] args) {
         System.out.println("Version 2");
 
-        /*
-        HOW WE CAN PUSH A GIT PROJECT TO GITHUB?- FOR THE FIRST TIME
-        Notes:
-        1- git init >> Create local repository. Create a .git folder. DO THIS ONCE FOR EACH PROJECT
-                NOTE: If git is not installed, then you will se an error.
-        2- git status (OPTIONAL) >> Shows changes. Our guide. Use anytime to see current condition.
-        3- git add .  >> add(stage) changes in Git repository for the next commit
-        4- git commit -m"any message" >> create a new commit(new version). Moves codes from Staging Area to Local Repository.
-        5- git branch -M main >> rename the current branch to main. DO THIS ONCE FOR EACH PROJECT
-        6- git remote add origin <repository-url> >> used to connect your local git to a remote repository. DO THIS ONCE FOR EACH PROJECT
-        7- git push -u origin main >> push the code to remote repository (github) DO THIS ONCE FOR EACH PROJECT
-         */
+
 
     }
 }
