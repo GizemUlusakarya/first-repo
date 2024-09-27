2af693033fd4504f901ef24f3bb137b960e75f24
GizemUlusakarya <gizem.techpro@gmail.com>
2024-09-27 17:59:04 +0100

    version2

--- src/main/java/first_git_package/MyGitClass3.java
+++ src/main/java/first_git_package/MyGitClass3.java
@@ -1,9 +1,9 @@
 package first_git_package;
 
-public class MyGitClass {
+public class MyGitClass3 {
 
     public static void main(String[] args) {
-        System.out.println("Version 1");
+        System.out.println("Version 3");
 
         /*
         HOW WE CAN PUSH A GIT PROJECT TO GITHUB?- FOR THE FIRST TIME
