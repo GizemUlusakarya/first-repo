6205a2e5d48a46965c0201c848114496bee66495
GizemUlusakarya <gizem.techpro@gmail.com>
2024-09-28 18:00:06 +0100

    day2 v3

--- src/main/java/first_git_package/MyGitClass5.java
+++ src/main/java/first_git_package/MyGitClass5.java
@@ -2,6 +2,6 @@ package first_git_package;
 
 public class MyGitClass5 {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hi world");
     }
 }
